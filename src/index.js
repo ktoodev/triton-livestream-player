3d4ede6c340ea2bc254fde149f4f4c887a867540
davidmpurdy <davidpurdy@gmail.com>
2020-07-07 16:57:00 -0600

    Resolved <script> tag detection issue (value now hard-coded); minor styling updates to work better on pages with their own styles

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import $ from "jquery";
 import {Triton_Livestream_Player} from './Triton_Livestream_Player';
 
 
-/* when the document is ready, set things up */
+// when the document is ready, set things up
 $(document).ready(function() {
 
-  /* Load the Triton SDK and create our player object when it's loaded */
+  // Load the Triton SDK and create our player object when it's loaded
   $.getScript('//sdk.listenlive.co/web/2.9/td-sdk.min.js')
     .done(function(script, textStatus) {
 
-      const current_script = document.scripts[document.scripts.length - 1];
 
-      const player_id = $(current_script).data('player-id');
+      const player_id = 'livestream_player';
 
       new Triton_Livestream_Player(player_id);
     });
