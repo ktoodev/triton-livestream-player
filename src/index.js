7dc33d7f10df1f4bd2374ed65f4b785969ac6f39
David Purdy <davidpurdy@gmail.com>
2021-04-26 17:05:44 -0600

    Update SDK version to 2.9.41

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import '../vendor/triton-player-sdk/2.9.34/td-sdk.min.js';
+import '../vendor/triton-player-sdk/2.9.41/td-sdk.min.js';
 
 import {Triton_Livestream_Player} from './Triton_Livestream_Player';
 
-new Triton_Livestream_Player('livestream_player'); 
-
+new Triton_Livestream_Player('livestream_player');
